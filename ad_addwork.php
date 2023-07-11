e9f8360eb4423550005290bdc5012f9fe70ece21
khunton2 <pasit555tonson@gmail.com>
2023-07-11 21:17:35 +0700

    addimg

--- ad_addwork.php
+++ ad_addwork.php
@@ -1,3 +1,30 @@
+<?php
+session_start();
+echo '
+<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">';
+//เช็คว่ามีตัวแปร session อะไรบ้าง
+//print_r($_SESSION);
+//exit();
+//สร้างเงื่อนไขตรวจสอบสิทธิ์การเข้าใช้งานจาก session
+if (empty($_SESSION['id']) && empty($_SESSION['name'])) {
+    echo '<script>
+                setTimeout(function() {
+                swal({
+                title: "คุณไม่มีสิทธิ์ใช้งานหน้านี้กรุณาเข้าสู่ระบบ",
+                
+                type: "error"
+                }, function() {
+                window.location = "login_reg.php"; //หน้าที่ต้องการให้กระโดดไป
+                });
+                }, 1000);
+                </script>';
+    exit();
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <?php
